a369091ef127d0f07018d513213c62b9987f8a7e
Baek <74183179+Geunbaek@users.noreply.github.com>
2024-04-23 20:18:59 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 4 ms, Memory: 13204 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.rs
@@ -0,0 +1,18 @@
+use std::{
+    cmp, collections::{ BinaryHeap, HashMap, HashSet, VecDeque }, io::{ stdin, stdout, BufWriter, Read, Write }
+};
+
+
+fn main() {
+    let mut s = String::new();
+
+    stdin().read_to_string(&mut s);
+
+    let mut input = s.split_ascii_whitespace();
+    let mut output = BufWriter::new(stdout());
+
+    let n: usize = input.next().unwrap().parse().unwrap();
+    for i in 0..n {
+        writeln!(output, "{}", "*".repeat(n - i));
+    }
+}   
