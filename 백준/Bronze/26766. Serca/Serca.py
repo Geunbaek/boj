f48a80e706926e080c6d60a920b822fa8028def6
Baek <74183179+Geunbaek@users.noreply.github.com>
2024-06-02 10:45:16 +0900

    [Bronze V] Title: Serca, Time: 160 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.py
@@ -0,0 +1,13 @@
+n = int(input())
+heart = """ @@@   @@@ 
+@   @ @   @
+@    @    @
+@         @
+ @       @ 
+  @     @  
+   @   @   
+    @ @    
+     @     """
+
+for _ in range(n):
+  print(heart)
