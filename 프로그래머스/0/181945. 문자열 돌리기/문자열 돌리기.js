21a91418bc320c4502f33cdf9de5b9a61a9f59b9
Baek <74183179+Geunbaek@users.noreply.github.com>
2024-12-30 10:05:22 +0900

    [level 0] Title: 문자열 돌리기, Time: 43.84 ms, Memory: 32.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.js
@@ -0,0 +1,15 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = [line];
+}).on('close',function(){
+    const [str] = input;
+    
+    [...str].forEach((char) => console.log(char))
+});
