a763ab9bc4f5d67893ac58e06276dc47d5af1b92
Baek <74183179+Geunbaek@users.noreply.github.com>
2025-01-06 10:06:43 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 3.82 ms, Memory: 36.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.js
@@ -0,0 +1,12 @@
+function solution(arr, queries) {
+    const answer = [...arr];
+    
+    queries.forEach(([s, e, k]) => {
+        for(let i = s; i <= e; i ++) {
+            if (i % k === 0) {
+                answer[i] += 1;
+            }
+        }
+    })
+    return answer;
+}
