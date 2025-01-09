2bd8dc15659e1e0f7d2d19e5a48d4b718ef64293
Baek <74183179+Geunbaek@users.noreply.github.com>
2025-01-09 10:50:50 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.57 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.js
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.js
@@ -1,22 +1,26 @@
 function solution(my_string) {
     const ret = Array.from({length: 52}, () => 0);
+        
+//     const lowerAlphaCache = Array.from(
+//         {length: 26},
+//         (_, index) => String.fromCharCode('a'.charCodeAt(0) + index)
+//     ).reduce((acc, cur, i) => ({...acc, [cur]: i + 26}), {});
     
-    const lowerAlphaCache = Array.from(
-        {length: 26},
-        (_, index) => String.fromCharCode('a'.charCodeAt(0) + index)
-    ).reduce((acc, cur, i) => ({...acc, [cur]: i + 26}), {});
+//     const upperAlphaCache = Array.from(
+//         {length: 26}, 
+//         (_, index) => String.fromCharCode('A'.charCodeAt(0) + index)
+//     ).reduce((acc, cur, i) => ({...acc, [cur]: i}), {});
     
-    const upperAlphaCache = Array.from(
-        {length: 26}, 
-        (_, index) => String.fromCharCode('A'.charCodeAt(0) + index)
-    ).reduce((acc, cur, i) => ({...acc, [cur]: i}), {});
+//     console.log(lowerAlphaCache, upperAlphaCache);
     
     [...my_string]
         .forEach((char) => {
             if (char.toLowerCase() === char) {
-                ret[lowerAlphaCache[char]] += 1 
+                const index = char.charCodeAt(0) - 'a'.charCodeAt(0) + 26;
+                ret[index] += 1 
             } else {
-                ret[upperAlphaCache[char]] += 1 
+                const index = char.charCodeAt(0) - 'A'.charCodeAt(0);
+                ret[index] += 1 
             }
         })
     return ret
