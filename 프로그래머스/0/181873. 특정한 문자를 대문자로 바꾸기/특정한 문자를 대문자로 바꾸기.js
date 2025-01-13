b26c9625f7f97c4c39c6f110d9878850b3d49f7e
Baek <74183179+Geunbaek@users.noreply.github.com>
2025-01-13 10:08:57 +0900

    [level 0] Title: 특정한 문자를 대문자로 바꾸기, Time: 0.19 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.js
@@ -0,0 +1,8 @@
+function solution(my_string, alp) {
+    return [...my_string]
+        .map(char => 
+             char === alp 
+             ? char.toUpperCase()
+             : char)
+        .join("")
+}
