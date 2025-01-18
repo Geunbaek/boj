c45a1d5940a4429a6553237ab768c447efe44bd8
Baek <74183179+Geunbaek@users.noreply.github.com>
2025-01-18 10:27:32 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.js
@@ -0,0 +1,11 @@
+function solution(a, b) {
+    if (a % 2 === 0 && b % 2 === 0){
+        return Math.abs(a - b);    
+    }
+    
+    if (a % 2 !== 0 && b % 2 !== 0) {
+        return a * a + b * b;        
+    }
+    
+    return 2 * (a + b);
+}
