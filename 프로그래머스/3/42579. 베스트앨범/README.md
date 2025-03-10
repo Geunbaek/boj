fc7f8e766c1df235a26fbc9ce6684ba20a8220a7
Baek <74183179+Geunbaek@users.noreply.github.com>
2025-03-10 22:05:49 +0900

    [level 3] Title: 베스트앨범, Time: 0.57 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/README.md
+++ 프로그래머스/3/42579. 베스트앨범/README.md
@@ -1,10 +1,10 @@
 # [level 3] 베스트앨범 - 42579 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 0.37 ms
+메모리: 33.6 MB, 시간: 0.57 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 10일 20:04:02
+2025년 03월 10일 22:05:47
 
 ### 문제 설명
 
--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
@@ -1,7 +1,13 @@
 function solution(genres, plays) {
     const n = genres.length;
+    
+    // Map<genre, number>
+    // 노래가 가장 많이 재생된 장르를 찾기 위함
     const genrePlaysMap = new Map();
+    // Map<genre, {play: number, index: numer}[]>
+    // 장르 내에서 가장 많이 재생된 노래를 찾기 위함
     const genreSongsMap = new Map();
+    
     
     for (let index = 0; index < n; index ++) {
         const genre = genres[index];
@@ -10,12 +16,15 @@ function solution(genres, plays) {
         genrePlaysMap.set(genre, (genrePlaysMap.get(genre) || 0) + play)
         genreSongsMap.set(genre, (genreSongsMap.get(genre) || []).concat({play, index}))
     }
+    
+    // 노래가 재생된 횟수에 따라 내림차순 정렬
     const sortedGenrePlays = [...genrePlaysMap.entries()].sort((a, b) => b[1] - a[1]);
     
+    // 정렬된 장르에 따라 속한 노래의 재생된 횟수에 따라 내림차순 정렬후 가장 많이 재생된 2곡의 index 를 추출
     const answer = sortedGenrePlays.map(([genre]) => {
         return genreSongsMap
             .get(genre)
-            .sort((a, b) => b.play - a.play)
+            .sort((a, b) => b.play - a.play || a.index - b.index)
             .slice(0, 2)
             .map(song => song.index);
     }).flat();
