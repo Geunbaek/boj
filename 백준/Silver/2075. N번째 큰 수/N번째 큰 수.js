15f171e608a73d42e0444583b42a734223a3064d
Baek <74183179+Geunbaek@users.noreply.github.com>
2025-03-24 16:57:23 +0900

    [Silver III] Title: N번째 큰 수, Time: 1188 ms, Memory: 18464 KB -BaekjoonHub

---
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.js
@@ -0,0 +1,26 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let n = -1;
+const heap = [];
+
+rl.on("line", (input) => {
+  if (n === -1) {
+    n = Number(input);
+    return;
+  } else {
+    const temp = input.split(" ");
+    for (let i = 0; i < temp.length; i++) {
+      heap.push(Number(temp[i]));
+    }
+
+    heap.sort((a, b) => b - a);
+    while (heap.length > n) heap.pop();
+  }
+}).on("close", () => {
+  console.log(heap[n - 1]);
+});
