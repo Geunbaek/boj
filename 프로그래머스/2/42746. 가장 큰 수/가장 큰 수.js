59f1d60ebc578a750cc19346b5e6aa169c4c6771
Baek <74183179+Geunbaek@users.noreply.github.com>
2025-03-31 21:47:57 +0900

    [level 2] Title: 가장 큰 수, Time: 520.55 ms, Memory: 45.1 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,5 +1,6 @@
 const compare = (a, b) => {
-    return `${b}${a}` - `${a}${b}`
+    return `${b}${a}` - `${a}${b}`;
+    // return b.toString().repeat(6).slice(6) - a.toString().repeat(6).slice(6);
 }
 
 function solution(numbers) {
