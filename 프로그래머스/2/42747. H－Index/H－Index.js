7a3885261b416433e7c2fa022e1c2d370350df20
Baek <74183179+Geunbaek@users.noreply.github.com>
2025-03-31 18:38:18 +0900

    [level 2] Title: H-Index, Time: 0.45 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.js
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -1,19 +1,18 @@
 /*
-h-index : 발표한 논문 수, 인용횟수 둘 중 최소값
+h-index : a 번 이상 인용된 논문이 b 편 이상일 때 a와 b의 최솟값 중 최대값
 */
 
+
 function solution(citations) {
+    let maxHIndex = 0;
     citations.sort((a, b) => a - b);
     
     const n = citations.length;
     for (let i = 0; i < n; i ++) {
         const citation = citations[i];
-        const restCite = n - i;
-        
-        if (citation >= restCite) {
-            return restCite;
-        }
+        const hIndex = Math.min(citation, n - i);
+        maxHIndex = Math.max(maxHIndex, hIndex);
     }
     
-    return 0;
+    return maxHIndex;
 }
