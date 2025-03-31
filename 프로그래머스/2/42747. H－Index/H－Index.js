928c0607bfbf2712fa05310936a88a7be6d6eadd
Baek <74183179+Geunbaek@users.noreply.github.com>
2025-03-31 18:00:31 +0900

    [level 2] Title: H-Index, Time: 0.46 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.js
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -1,10 +1,18 @@
+/*
+발표한 논문, 피인용횟수 둘 중 최소값이 h-index 입니다.
+*/
+
+
 function solution(citations) {
-    let answer = 0;
+    let maxHIndex = 0;
     citations.sort((a, b) => b - a);
+    
     for (let i = 0; i < citations.length; i ++) {
         const citation = citations[i];
-        const hIndex = Math.min(i + 1, citation);
-        answer = Math.max(answer, hIndex);
+        const hIndex = Math.min(citation, i + 1)
+   
+        maxHIndex = Math.max(maxHIndex, hIndex);
     }
-    return answer;
+    
+    return maxHIndex;
 }
--- 프로그래머스/2/42747. H－Index/README.md
+++ 프로그래머스/2/42747. H－Index/README.md
@@ -1,10 +1,10 @@
 # [level 2] H-Index - 42747 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747#qna) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 0.78 ms
+메모리: 33.5 MB, 시간: 0.46 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 22일 11:41:20
+2025년 03월 31일 18:00:29
 
 ### 문제 설명
 
