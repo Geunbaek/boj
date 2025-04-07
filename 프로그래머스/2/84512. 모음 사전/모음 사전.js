cf5d53da5dafd6209f0b786ddb9429a66f527251
Baek <74183179+Geunbaek@users.noreply.github.com>
2025-04-07 21:27:34 +0900

    [level 2] Title: 모음 사전, Time: 3.70 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.js
@@ -18,8 +18,51 @@ const recur = (depth, path, words) => {
     
 }
 
-function solution(word) {
+const recurSolution = (word) => {
     const words = {};
     recur(0, "", words)
     return words[word];
+}
+
+
+const forSolution = (word) => {
+    const words = {};
+    let SIZE = 0;
+
+    for (let i = 0; i < ALPHA.length; i++) {
+        let w1 = ALPHA[i];
+        words[w1] = SIZE + 1;
+        SIZE += 1;
+        
+        for (let j = 0; j < ALPHA.length; j++) {
+            let w2 = w1 + ALPHA[j];
+            words[w2] = SIZE + 1;
+            SIZE += 1;
+            
+            for (let k = 0; k < ALPHA.length; k++) {
+                let w3 = w2 + ALPHA[k];
+                words[w3] = SIZE + 1;
+                SIZE += 1;
+                
+                for (let l = 0; l < ALPHA.length; l++) {
+                    let w4 = w3 + ALPHA[l];
+                    words[w4] = SIZE + 1;
+                    SIZE += 1;
+                    
+                    for (let m = 0; m < ALPHA.length; m++) {
+                        let w5 = w4 + ALPHA[m];
+                        words[w5] = SIZE + 1;
+                        SIZE += 1;
+                    }
+                }
+            }
+        }
+    }
+
+    return words[word];
+}
+
+function solution(word) {
+    // return recurSolution(word) 
+    return forSolution(word)
 }
