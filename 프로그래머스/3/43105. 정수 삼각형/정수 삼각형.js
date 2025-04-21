fd6c0bf5e08272c59dc8363082d0c6a370447d54
Baek <74183179+Geunbaek@users.noreply.github.com>
2025-04-21 17:48:49 +0900

    [level 3] Title: 정수 삼각형, Time: 25.43 ms, Memory: 40.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
@@ -0,0 +1,18 @@
+function solution(triangle) {
+    const row = triangle.length;
+    
+    for (let y = 1; y < row; y ++) {
+        const col = triangle[y].length;
+        
+        for (let x = 0; x < col; x++) {
+            if (x === 0) {
+                triangle[y][x] += triangle[y - 1][x];
+            } else if (x === col - 1) {
+                triangle[y][x] += triangle[y - 1][x - 1];
+            } else {
+                triangle[y][x] += Math.max(triangle[y - 1][x - 1], triangle[y - 1][x]);
+            }
+        }
+    }
+    return Math.max(...triangle.at(-1));
+}
